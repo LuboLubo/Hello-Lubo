d6b058048bbb5d9aad86a26ec886d89aed871dd7
LuboLubo <lubomir.code@gmail.com>
2017-01-28 23:19:49 +0200

    Create new file
    да не би да стана

---
+++ Programming_Basics_Exam_17_January_2016/Problem_01_Cake_Tycoon/Cake Tycoon.cs
@@ -0,0 +1,16 @@
+﻿namespace Problem_01_Cake_Tycoon
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ Programming_Basics_Exam_17_January_2016/Problem_02_Beer _tock/Beer _tock.cs
@@ -0,0 +1,16 @@
+﻿namespace Problem_02_Beer__tock
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloBeer");
+        }
+    }
+}
